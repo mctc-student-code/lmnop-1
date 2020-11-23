9ea779613c02c5075e887fbb6e38da8c5e4c3e61
Thomas Mullins <mn4774jm@go.minneapolis.edu>
2020-11-23 14:31:09 -0600

    added test_paginate to test module.

---
+++ lmn/tests/test_paginate.py
@@ -0,0 +1,13 @@
+from django.test import TestCase
+
+
+class TestArtistsListWithPageData(TestCase):
+    pass
+
+
+class TestVenuesListWithPageData(TestCase):
+    pass
+
+
+class TestNotesListWithPageData(TestCase):
+    pass
