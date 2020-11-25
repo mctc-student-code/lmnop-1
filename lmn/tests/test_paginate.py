a9b7ed4130637b87ce8d82c76ebc112e58a74751
Thomas Mullins <mn4774jm@go.minneapolis.edu>
2020-11-24 20:02:51 -0600

    started test for page number out of bounds

--- lmn/tests/test_paginate.py
+++ lmn/tests/test_paginate.py
@@ -13,23 +13,33 @@ class TestListWithPageData(TestCase):
             venue = Venue(name=f'venue{i+1}', city=f'city{i+1}', state=f'state{i+1}')
             venue.save()
 
+    # check that names returned from db are correct on first loaded page
     def test_first_page_correct_entries(self):
         artists = Artist.objects.all().order_by('name')
         paged_list = helpers.pg_records(1, artists, 2)
         self.assertEqual(artists[0].name, paged_list[0].name)
         self.assertEqual(artists[1].name, paged_list[1].name)
 
+    # check that names returned from db are correct on second loaded page
     def test_second_page_correct_entries(self):
         artists = Artist.objects.all().order_by('name')
         paged_list = helpers.pg_records(2, artists, 2)
         self.assertEqual(artists[2].name, paged_list[0].name)
 
+    # check that the desired number of items are displayed per page
     def test_correct_number_of_items_returned_page_one(self):
         artists = Artist.objects.all().order_by('name')
         paged_list = helpers.pg_records(1, artists, 2)
         self.assertEqual(len(paged_list), 2)
 
+    # check that the desired number of items are displayed per page
     def test_correct_number_of_items_returned_page_two(self):
         artists = Artist.objects.all().order_by('name')
         paged_list = helpers.pg_records(2, artists, 2)
         self.assertEqual(len(paged_list), 1)
+
+    # check that goes to last page
+    def test_page_number_higher_than_exists(self):
+        artists = Artist.objects.all().order_by('name')
+        paged_list = helpers.pg_records(4000, artists, 2)
+        #TODO compare page number returned to last available page number
